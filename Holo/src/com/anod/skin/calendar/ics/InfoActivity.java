a9da26d8d6f1bbd0af7228d409ed22266ab9131c
anod <alex.gavrishev@gmail.com>
2012-05-27 22:43:24 +0300

    add Holo theme

---
+++ Holo/src/com/anod/skin/calendar/ics/InfoActivity.java
@@ -0,0 +1,14 @@
+package com.anod.skin.calendar.ics;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class InfoActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+    }
+}
